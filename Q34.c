0b78b4d8c0fad45e19cc47fbf2477afa80a43c99
tvtriyasha <varshneytriyasha@gmail.com>
2025-09-21 23:43:20 +0530

    Question 34

--- Q34.c
+++ Q34.c
@@ -14,16 +14,22 @@ Not prime
 */
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int num, i, isPrime = 1;
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (num <= 1) {
-        isPrime = 0; // 0 and 1 are not prime
-    } else {
-        for (i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
+    if (num <= 1) 
+    {
+        isPrime = 0;
+    } 
+    else 
+    {
+        for (i = 2; i * i <= num; i++) 
+        {
+            if (num % i == 0) 
+            {
                 isPrime = 0;
                 break;
             }
