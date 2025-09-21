5e82463a83aa0a190ceb42337ca2a20cd2597d14
tvtriyasha <varshneytriyasha@gmail.com>
2025-09-22 03:16:28 +0530

    Question 46

---
+++ Q46.c
@@ -0,0 +1,36 @@
+/*Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include <stdio.h>
+
+int main() 
+{
+    int rows = 5;
+
+    for(int i = 1; i <= rows; i++) 
+    {
+        for(int j = 1; j <= rows; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
