1f895a1e508469f1c51056ad2bd977031aa7dc55
tvtriyasha <varshneytriyasha@gmail.com>
2025-10-03 22:12:26 +0530

    Question 86

--- Q86.c
+++ Q86.c
@@ -15,29 +15,17 @@ Not palindrome
 #include <stdio.h>
 #include <string.h>
 
-int main() 
-{
+int main() {
     char str[100];
-    char rev[100];
-    int i, j, len;
-    int isPalindrome = 1;
+    int i, j, len, isPalindrome = 1;
 
     printf("Enter a string: ");
     scanf("%s", str);
 
     len = strlen(str);
 
-    for (i = 0, j = len - 1; i < len; i++, j--) 
-    {
-        rev[i] = str[j];
-    }
-    rev[len] = '\0';
-    printf("Reversed string: %s\n", rev);
-
-    for (i = 0; i < len; i++) 
-    {
-        if (str[i] != rev[i]) 
-        {
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        if (str[i] != str[j]) {
             isPalindrome = 0;
             break;
         }
@@ -46,7 +34,7 @@ int main()
     if (isPalindrome)
         printf("Palindrome\n");
     else
-        printf("Not Palindrome\n");
+        printf("Not palindrome\n");
 
     return 0;
 }
